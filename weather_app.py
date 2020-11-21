607b1e4ad7e8387e9d15ec637c458c386d7e1e7d
Emmanuel Jerry <62827201+Emmzy17@users.noreply.github.com>
2020-11-21 03:56:36 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -1,83 +1,62 @@
-import tkinter as tk
-from tkinter import font
-import requests
+#a809ed50df638e21c97d4ba5af7e917
 
-root = tk.Tk()
+import tkinter as tk
+import requests
 
 HEIGHT = 500
 WIDTH = 600
 
+def test_function(entry):
+	print("This is the entry:", entry)
+
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# a4aa5e3d83ffefaba8c00284de6ef7c3
 
 def format_response(weather):
-    try:
-        name =(weather['name'])
-        desc = (weather[0]['description'])
-        temp = (weather['main']['temp'])
+	try:
+		name = weather['name']
+		desc = weather['weather'][0]['description']
+		temp = weather['main']['temp']
 
-        final_str = " city: %s \nCondition: %S \nTemperate(F): %s" % (name, desc,temp)
-    except:
-        final_str = 'There was a Problem Finding that city' 
-        return final_str
+		final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (name, desc, temp)
+	except:
+		final_str = 'There was a problem retrieving that information'
+
+	return final_str
 
 def get_weather(city):
-    weather_key = 'a809ed50df638e21c97d4ba5af7e917'
-    url = 'https://api.openweathermap.org/data/2.5/'
-    params = {'APPID': weather_key, 'q':city, 'units':'imperial'}
-    response = requests.get(url, params = params)
-    weather = response.json()
+	weather_key = 'a809ed50df638e21c97d4ba5af7e917'
+	url = 'https://api.openweathermap.org/data/2.5/weather'
+	params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+	response = requests.get(url, params=params)
+	weather = response.json()
 
-    label['text'] = format_response(weather)
-   
-canvas = tk.Canvas(root, height = HEIGHT, width= WIDTH)
+	label['text'] = format_response(weather)
+
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-background = tk.PhotoImage(file = 'landscape.png')
-background_image = tk.Label(root, image = background)
-background_image.place(relwidth = 1, relheight=1)
+background_image = tk.PhotoImage(file='landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
-frame = tk.Frame(root, bg='#80c1ff', bd = 5)
-frame.place(relx = 0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor = 'n')
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-entry = tk.Entry(frame, bg='white', font=('courier', 18) )
-entry.place(relwidth = 0.65, relheight=1)
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", command= lambda: get_weather(entry.get()), font=('courier', 12))
-button.place(relx = 0.7, relheight=1, relwidth=0.3)
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
 
-lower_frame = tk.Frame(root, bg="#80c1ff", bd = 5)
-lower_frame.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight=0.6, anchor = 'n')
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-label = tk.Label(lower_frame, anchor = 'nw', justify ='left', font=('courier', 18) ) 
-label.place( relwidth=1, relheight=1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
 
 root.mainloop()
-
-
-
-
-
-
-#After this program create a GUI guessing app
